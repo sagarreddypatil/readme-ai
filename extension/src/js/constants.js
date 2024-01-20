ec87d241cc1bcc580b9d06a9c84b634341ae3dee
Sagar Patil <sagarreddypatil@gmail.com>
2024-01-20 16:53:03 -0500

    that one sucked

--- extension/src/js/constants.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-unused-vars */
-const exbrowser = "chrome";
-// values: "chrome", "safari", "edge", "firefox", "opera", "yandex", "whale"
-const browsernewtab = "chrome://newtab/";
-const browserstore = "https://chrome.google.com";
